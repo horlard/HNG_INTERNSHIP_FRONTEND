ebb79c614026c5f8e9b88fd1a3559676c27a4297
Niscolinx <munisco12@gmail.com>
2019-09-16 11:33:47 +0100

    updated the signup page

--- src/pages/auth/sign_up.js
+++ src/pages/auth/sign_up.js
@@ -1,72 +1,58 @@
-import React, { Component } from 'react';
-import classes from './signup.module.css';
+import React, { Component } from 'react'
+import classes from './signup.module.css'
 
 export default class Signup extends Component {
-  state = {
-    authStart: false
-  };
-  render() {
-    return (
-      <form className={classes.form}>
-        <h1 className={classes.header}>{this.state.authStart ? 'CREATE PASSWORD' : 'SIGN UP'}</h1>
-        {this.state.authStart ? (
-          <div className={classes.inputcontainer}>
-            <div className={classes.input}>
-              <span>Password</span>
-              <input placeholder="password" type="password" />
-            </div>
-            <div className={classes.input}>
-              <span>Confirm Password</span>
-              <input placeholder="confirm password" type="password" />
-            </div>
-            <button
-              type="submit"
-              onSubmit={e => {
-                e.preventDefault();
-              }}
-            >
-              SIGN UP
-            </button>
-          </div>
-        ) : (
-          <div className={classes.inputcontainer}>
-            <div className={classes.input}>
-              <span>First Name</span>
-              <input placeholder="John" />
-            </div>
-            <div className={classes.input}>
-              <span>Last Name</span>
-              <input placeholder="Doe" />
-            </div>
-            <div className={classes.input}>
-              <span>Email</span>
-              <input placeholder="Aa@domain.com" />
-            </div>
-            <span className={classes.policy}>
-              By clicking continue, you're onboard with our
-              <span
-                style={{
-                  color: '#0073b1',
-                  fontWeight: 'bold',
-                  fontFamily: 'Ubuntu'
-                }}
-              >
-                {' '}
-                privacy policy.
-              </span>
-            </span>
-            <button
-              type="submit"
-              onClick={e => {
-                e.preventDefault();
-                this.setState({ authStart: true });
-              }}
-            >
-              CONTINUE
-            </button>
-          </div>
-        )}
-      </form>
-    );
-  }
+    state = {
+        authStart: false
+    }
+    render() {
+        return (
+            <form className={classes.form}>
+                <div className={classes.inputcontainer}>
+                    <div className={classes.input}>
+                        <span>First Name</span>
+                        <input placeholder="John" />
+                    </div>
+                    <div className={classes.input}>
+                        <span>Last Name</span>
+                        <input placeholder="Doe" />
+                    </div>
+                    <div className={classes.input}>
+                        <span>Email</span>
+                        <input placeholder="Aa@domain.com" />
+                    </div>
+                    <div className={classes.input}>
+                        <span>Password</span>
+                        <input placeholder="password" type="password" />
+                    </div>
+                    <div className={classes.input}>
+                        <span>Confirm Password</span>
+                        <input placeholder="confirm password" type="password" />
+                    </div>
+                    <span className={classes.policy}>
+                        By clicking signup, you're onboard with our
+                        <span
+                            style={{
+                                color: '#0073b1',
+                                fontWeight: 'bold',
+                                fontFamily: 'Ubuntu'
+                            }}
+                        >
+                            {' '}
+                            privacy policy.
+                        </span>
+                    </span>
+                    <button
+                        type="submit"
+                        onSubmit={e => {
+                            e.preventDefault()
+                            this.setState({ authStart: true })
+                        }}
+                    >
+                        SIGNUP
+                    </button>
+                </div>
+            </form>
+        )
+    }
 }
