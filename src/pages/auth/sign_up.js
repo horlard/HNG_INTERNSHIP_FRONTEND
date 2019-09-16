76f41a65a43ead6c622592a910bb3c36790591ab
Niscolinx <munisco12@gmail.com>
2019-09-16 12:42:32 +0100

    Added some basic validation to the signup page

--- src/pages/auth/sign_up.js
+++ src/pages/auth/sign_up.js
@@ -3,31 +3,67 @@ import classes from './signup.module.css'
 
 export default class Signup extends Component {
     state = {
-        authStart: false
+        authStart: false,
+      FirstName: '',
+      LastName: '',
+      email: '',
+      password: '',
+      confirmPass: ''
     }
+    onEmailInput = e => {
+        this.setState({ email: e.target.value })
+    }
+    onPasswordInput = e => {
+        this.setState({ password: e.target.value })
+    }
+    onFirstName = e => {
+        this.setState({ FirstName: e.target.value })
+    }
+    onLastName = e => {
+        this.setState({ LastName: e.target.value })
+    }
+    onPasswordConfirm = e => {
+        this.setState({ confirmPass: e.target.value })
+    }
+    
     render() {
         return (
             <form className={classes.form}>
                 <div className={classes.inputcontainer}>
                     <div className={classes.input}>
                         <span>First Name</span>
-                        <input placeholder="John" />
+                        <input required placeholder="John" minLength="4" onChange = {this.onFirstName}/>
                     </div>
                     <div className={classes.input}>
                         <span>Last Name</span>
-                        <input placeholder="Doe" />
+                        <input required placeholder="Doe" minLength="4" onChange = {this.onLastName} />
                     </div>
                     <div className={classes.input}>
                         <span>Email</span>
-                        <input placeholder="Aa@domain.com" />
+                        <input
+                            required
+                            placeholder="Aa@domain.com"
+                            type="email"
+                        onChange = {this.onEmailInput}/>
                     </div>
                     <div className={classes.input}>
                         <span>Password</span>
-                        <input placeholder="password" type="password" />
+                        <input
+                            required
+                            placeholder="password"
+                            type="password"
+                            minLength="6"
+                            onChange = {this.onPasswordInput}
+                        />
                     </div>
                     <div className={classes.input}>
                         <span>Confirm Password</span>
-                        <input placeholder="confirm password" type="password" />
+                        <input
+                            required
+                            placeholder="confirm password"
+                            type="password"
+                            onChange = {this.onPasswordConfirm}
+                        />
                     </div>
                     <span className={classes.policy}>
                         By clicking signup, you're onboard with our
