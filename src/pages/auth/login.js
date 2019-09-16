92fd2b08b3137d0cfc237f823f328d903374e6fa
horlard <oladipuposaulih@gmail.com>
2019-09-16 14:41:10 +0100

    login refactor

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -24,122 +24,59 @@ onPasswordInput=e=> {
 
  render() {
   return (
-   <form className={classes.form}>
-    {!this.state.respass ? (
-     <div>
-      <h1 className={classes.header}>LOGIN</h1>
-      <div className={classes.inputcontainer}>
-       <div className={classes.input}>
-        <span>Email</span>
-        <input placeholder="Aa@domain.com" onChange={this.onEmailInput}/>
-       </div>
-       <div className={classes.input}>
-        <span>Password</span>
-        <input placeholder="Password" type="password" onChange={this.onPasswordInput}/>
-       </div>
-       <button
-        type="submit"
-        onClick={e => {
-         e.preventDefault();
-         this.setState({ loading: true });
-        }}
-       >
-        LOGIN
-       </button>
-       <span className={classes.signup}>
-        New ?{' '}
-        <span
-         onClick={() => this.props.history.push('/signup')}
-        >
-         Sign Up
-        </span>
-       </span>
-       <span
-        className={classes.fgt}
-        onClick={() => this.setState({ respass: true })}
-       >
-        Forgot password ?
-       </span>
+   <div>
+       <div className="ui middle aligned center aligned grid" style={{transform:'translateY(50%)'}}>
+  <div className="column">
+    <h2 className="ui teal image header">
+      <div className="content">
+        Sign-up
       </div>
-     </div>
-    ) : !this.state.emailSent ? (
-     <div>
-      <h1 className={classes.header}>
-       Forgot your password?
-      </h1>
-      <p className={classes.emailSent}>
-       Enter your email address below and we'll email you
-       instructions on how to change your password.
-      </p>
-      <div className={classes.inputcontainer}>
-       <div className={classes.input}>
-        <span>Email</span>
-        <input placeholder="Enter your email address" />
-       </div>
-       <button
-        type="submit"
-        onClick={e => {
-         e.preventDefault();
-         this.setState({ emailSent: true });
-        }}
-       >
-        CONTINUE
-       </button>
+    </h2>
+    <form className="ui large form">
+      <div className="ui stacked segment">
+        <div className="field">
+          <div className="ui left icon input">
+            <i className="user icon"></i>
+            <input type="text" name="text" placeholder="First Name" onChange={this.onFirstName} required/>
+          </div>
+        </div>
+        <div className="field">
+          <div className="ui left icon input">
+            <i className="user icon"></i>
+            <input type="text" name="text" placeholder="Last Name" onChange={this.onLastName} required/>
+          </div>
+        </div>
+        <div className="field">
+          <div className="ui left icon input">
+            <i className="mail icon"></i>
+            <input type="email" name="text" placeholder="E-mail Address" onChange={this.onEmailInput} required/>
+          </div>
+        </div>
+        <div className="field">
+          <div className="ui left icon input">
+            <i className="lock icon"></i>
+            <input type="password" name="password" placeholder="Password" onChange={this.onPasswordInput} required/>
+          </div>
+        </div>
+        <div className="field">
+          <div className="ui left icon input">
+            <i className="lock icon"></i>
+            <input type="password" name="password" placeholder="Confirm Password" onChange={this.onPasswordConfirm} required/>
+          </div>
+        </div>
+        <div className="ui fluid large teal submit button">Sign Up</div>
       </div>
-     </div>
-    ) : !this.state.verificationSuccess ? (
-     <div>
-      <h1 className={classes.header}>Verify Email</h1>
-      <p className={classes.emailSent}>
-       A four digit verification pin have been sent to your
-       email, enter the pin below to continue.
-      </p>
-      <div className={classes.inputcontainer}>
-       <div className={classes.input}>
-        <input placeholder="Verification Pin" />
-       </div>
-       <button
-        type="submit"
-        onClick={e => {
-         e.preventDefault();
-         this.setState({ verificationSuccess: true });
-        }}
-       >
-        CONTINUE
-       </button>
-      </div>
-     </div>
-    ) : (
-     <div>
-      <h1 className={classes.header}>Reset Password</h1>
 
-      <div className={classes.inputcontainer}>
-       <div className={classes.input}>
-        <span>Password</span>
-        <input placeholder="Password" type="password" />
-       </div>
-       <div className={classes.input}>
-        <span>Confirm password</span>
-        <input placeholder="Password" type="password" />
-       </div>
-       <button
-        type="submit"
-        onClick={e => {
-         e.preventDefault();
-         this.setState({
-          respass: false,
-          emailSent: false,
-          verificationSuccess: false
-         });
-        }}
-       >
-        CHANGE PASSWORD
-       </button>
-      </div>
-     </div>
-    )}
-   </form>
-  );
+      <div className="ui error message"></div>
+
+    </form>
+
+    
+  </div>
+</div>
+
+   </div>
+  )
  }
 }
 
