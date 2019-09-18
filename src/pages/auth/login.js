d0924bd5c0d01fc8fbb5df78b3e1f4139568b030
horlard <oladipuposaulih@gmail.com>
2019-09-18 20:40:03 +0100

    handled ajax requests from the server

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,102 +1,114 @@
-import React, { Component } from 'react'
-import { withRouter, Link } from 'react-router-dom'
-import BackendApi from './backendApi';
-import qs from 'qs';
+import React, { Component } from "react";
+import { withRouter, Link } from "react-router-dom";
+import BackendApi from "./backendApi";
+import qs from "qs";
 
-import './sign.css'
+import "./sign.css";
 
 class Login extends Component {
-    state = {
-        respass: false,
-        emailSent: false,
-        verificationSuccess: false,
-        loading: false,
-        email: '',
-        password: '',
-        Err: ''
-    }
+  state = {
+    respass: false,
+    emailSent: false,
+    verificationSuccess: false,
+    loading: false,
+    email: "",
+    password: "",
+    Err: "",
+    isLoading: false
+  };
 
-    onEmailInput = e => {
-        this.setState({ email: e.target.value })
-    }
-    onPasswordInput = e => {
-        this.setState({ password: e.target.value })
-    }
-    onFormSubmit = e => {
-        e.preventDefault()
-        BackendApi.post('/index.php/login',qs.stringify({
-            email:this.state.email,
-            password:this.state.password
-    })).then(res=> {
-            console.log(res);
-        })
-        
-    }
+  onEmailInput = e => {
+    this.setState({ email: e.target.value });
+  };
+  onPasswordInput = e => {
+    this.setState({ password: e.target.value });
+  };
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.setState({ isLoading: true });
+    BackendApi.post(
+      "/index.php/login",
+      qs.stringify({
+        email: this.state.email,
+        password: this.state.password
+      })
+    ).then(res => {
+      console.log(res);
+      this.setState({ isLoading: false });
+      if (res.data.error === 1) {
+        return this.setState({
+          Err: "Invalid details or Account does not exist"
+        });
+      }
+      return "";
+    });
+  };
 
-    render() {
-        return (
-            <div style={{ transform: 'translateY(80%)' }} className="contain ">
-                <div className="ui middle aligned center aligned grid">
-                    <div className="column">
-                        <h2 className="ui teal image header">
-                            <div className="content">Login to your Account</div>
-                        </h2>
-                        <form className="ui large form error" onSubmit={this.onFormSubmit}>
-                            <div className="ui stacked segment">
-                                <div className="field">
-                                    <div className="ui left icon input">
-                                        <i className="mail icon"></i>
-                                        <input
-                                            type="email"
-                                            name="text"
-                                            placeholder="E-mail Address"
-                                            onChange={this.onEmailInput}
-                                            required
-                                        />
-                                    </div>
-                                </div>
-                                <div className="field">
-                                    <div className="ui left icon input">
-                                        <i className="lock icon"></i>
-                                        <input
-                                            type="password"
-                                            name="password"
-                                            placeholder="Password"
-                                            onChange={this.onPasswordInput}
-                                            required
-                                        />
-                                    </div>
-                                </div>
-
-                                <div className="">
-                                    {' '}
-                                    <button
-                                        type="submit"
-                                        className="ui fluid large teal submit button"
-                                    >
-                                        Login
-                                    </button>
-                                </div>
-                            </div>
-
-                            <div
-                                className="ui error message"
-                                type="submit"
-                                onClick={this.onFormSubmit}
-                            >
-                                {this.state.Err}
-                            </div>
-                        </form>
-                        <div className="ui message">
-                            <p style={{ fontSize: '1.1rem' }}>
-                                New to us? <Link to="/signup">Sign Up</Link>{' '}
-                            </p>
-                        </div>
-                    </div>
+  render() {
+    return (
+      <div style={{ transform: "translateY(80%)" }} className="contain ">
+        <div className="ui middle aligned center aligned grid">
+          <div className="column">
+            <h2 className="ui teal image header">
+              <div className="content">Login to your Account</div>
+            </h2>
+            <form className="ui large form error" onSubmit={this.onFormSubmit}>
+              <div className="ui stacked segment">
+                <div className="field">
+                  <div className="ui left icon input">
+                    <i className="mail icon"></i>
+                    <input
+                      type="email"
+                      name="text"
+                      placeholder="E-mail Address"
+                      onChange={this.onEmailInput}
+                      required
+                    />
+                  </div>
                 </div>
+                <div className="field">
+                  <div className="ui left icon input">
+                    <i className="lock icon"></i>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      onChange={this.onPasswordInput}
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div className="">
+                  {" "}
+                  <button
+                    type="submit"
+                    className="ui fluid large teal submit button"
+                    disabled={this.state.isLoading}
+                  >
+                    {this.state.isLoading ? "Please Wait...." : "Login"}
+                  </button>
+                </div>
+              </div>
+
+              <div
+                className="ui error message"
+                type="submit"
+                onClick={this.onFormSubmit}
+              >
+                {this.state.Err}
+              </div>
+            </form>
+            <div className="ui message">
+              <p style={{ fontSize: "1.1rem" }}>
+                New to us? <Link to="/signup">Sign Up</Link>{" "}
+              </p>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default withRouter(Login)
+export default withRouter(Login);
