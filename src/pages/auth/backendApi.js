f2ce6264d9a8df1dde7c6d0459c19a2d0ad89cc6
horlard <oladipuposaulih@gmail.com>
2019-09-19 10:56:31 +0100

    changed baseURL

--- src/pages/auth/backendApi.js
+++ src/pages/auth/backendApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://cors-anywhere.herokuapp.com/https://intense-lowlands-41245.herokuapp.com',
+    baseURL:'https://cors-anywhere.herokuapp.com/https://whispering-everglades-26046.herokuapp.com',
     headers: {
         "Content-type": "application/x-www-form-urlencoded"
     }
