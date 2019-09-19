f6d9f17d6a10bf3d3367ff5e364f6af811c5f429
horlard <oladipuposaulih@gmail.com>
2019-09-19 11:20:28 +0100

    shows a welcome page after the user logs in successfully

---
+++ src/pages/auth/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sign.css';
+
+
+const Welcome=()=> {
+    return (
+        <div className='container'>
+            <h1 className='ui header'>Welcome</h1>
+        </div>
+    )
+}
+export default Welcome;
